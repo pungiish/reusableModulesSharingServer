5e96a44b9ce84c3c30ef05db220fc6d12bd730da
jan pungarsek <jan.pungarsek@gmail.com>
2019-08-21 13:47:30 +0200

    widgets work

---
+++ reusable_modules_sharing_server/Migrations/20190821090645_addedTagProperty.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WidgetServer.Migrations
+{
+    public partial class addedTagProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tag",
+                table: "Widget",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tag",
+                table: "Widget");
+        }
+    }
+}
